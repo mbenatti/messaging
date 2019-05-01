629cf5b384eeb4def559c20ef18dd96367e6f63e
Marcos Benatti Lauer <benatti77@gmail.com>
2019-05-01 16:05:37 -0300

    feature: message job

--- apps/core/lib/messaging/core/message_job.ex
+++ apps/core/lib/messaging/core/message_job.ex
@@ -1,3 +1,29 @@
 defmodule Messaging.Core.MessageJob do
+  @moduledoc """
+  Async `Task` to Process the Message
+  """
 
+  use Task
+
+  require Logger
+
+  @doc """
+  Start the MessageJob
+  """
+  @spec start(String.t(), String.t()) :: Task.t()
+  def start(queue, msg) do
+    Task.Supervisor.async_nolink(Messaging.MessageJobSupervisor, __MODULE__, :run, [queue,msg])
+  end
+
+  @doc """
+  Do whatever you want with the message
+  """
+  @spec run(String.t(), String.t()) :: :ok
+  def run(queue, msg) do
+    # Processing the message
+    # Some logic ex. save on db, etc.
+    Logger.info("MessageJob - Queue: #{queue} -> Msg: #{msg}}")
+
+    :ok
+  end
 end
