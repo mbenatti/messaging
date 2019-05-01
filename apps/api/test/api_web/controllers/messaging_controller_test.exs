cf1a4996cea6b4e0c2927200d15d42e3f87c94d3
Marcos Benatti Lauer <benatti77@gmail.com>
2019-05-01 18:15:24 -0300

    test: receive-message endpoint

--- apps/api/test/api_web/controllers/messaging_controller_test.exs
+++ apps/api/test/api_web/controllers/messaging_controller_test.exs
@@ -1,14 +1,23 @@
 defmodule Messaging.APIWeb.MessagingControllerTest do
   use Messaging.APIWeb.ConnCase, async: true
 
+  @path "/receive-message"
 
   describe "receive message" do
     test "success", %{conn: conn} do
-      
+      %Plug.Conn{status: status, resp_body: resp} = get(conn, @path, %{"queue" => "my_queue", "message" => "my_msg"})
+
+      %{"message" => resp_message} = Jason.decode!(resp)
+      assert resp_message =~ "Message received!"
+      assert status == 200
     end
 
     test "fail", %{conn: conn} do
+      %Plug.Conn{status: status, resp_body: resp} = get(conn, @path)
 
+      %{"message" => resp_message} = Jason.decode!(resp)
+      assert resp_message =~ "Invalid params"
+      refute status == 200
     end
   end
 
