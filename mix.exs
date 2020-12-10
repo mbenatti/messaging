c8e9732f7df7f4f8e5b106ac839c1c070b5bad12
Marcos Benatti Lauer <benatti77@gmail.com>
2020-12-10 11:56:00 -0300

    feat: add code analysis

--- mix.exs
+++ mix.exs
@@ -9,12 +9,7 @@ defmodule Messaging.MixProject do
     ]
   end
 
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [{:credo, "~> 1.5"}]
   end
 end
